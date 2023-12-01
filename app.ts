9896f2d7b5e7ae377ac1334921cd516946d485e6
johnmcg04 <john.mcglinchey@kainos.com>
2023-12-01 13:38:16 +0000

    general cghanges

--- app.ts
+++ app.ts
@@ -1,60 +1,30 @@
-import { Response, Request } from 'express';
+import { Request, Response } from "express";
 
-const express = require('express');
+const express = require("express");
 const path = require("path");
 const nunjucks = require("nunjucks");
-const { title } = require('process');
-const session = require('express-session');
-
-
 
 const app = express();
 
-//configure nunjucks
-let appViews = path.join(__dirname, '/views/');
+// Configure Nunjucks
+const appViews = path.join(__dirname, "/views/");
 
-let nunjucksConfig = {
+const nunjucksConfig = {
     autoescape: true,
     noCache: true,
     express: app
 };
 
-
 nunjucks.configure(appViews, nunjucksConfig);
 
-//configure express
-
-app.set('view engine', 'html');
-
-app.use("/public", express.static(path.join(__dirname, "public")));
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session(
-        {   
-            secret:'not hard coded secret', 
-            cookie:{ maxAge: 60000 }
-        }
-    )
-);
-
-declare module "express-session" {
-    interface SessionData{
-        token: String;
-    }
-    
-}
+// Configure Express
+app.set("view engine", "html");
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
 });
 
-//express routes
-
-app.get('/', (req: Request, res: Response) => {
-    res.render("academyExperience", {
-        title: 'Academy title',
-    });     
+// Express Routes
+app.get("/", (req: Request, res: Response) => {
+    res.render("index");
 });
