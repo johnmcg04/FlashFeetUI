a665465244ac1536810e40df25eec2ed256eb721
johnmcg04 <john.mcglinchey@kainos.com>
2023-12-19 22:34:54 +0000

    Updated testing

--- test/ui/LoginTest.js
+++ test/ui/LoginTest.js
@@ -1,71 +1,42 @@
-//const webdriver = require('selenium-webdriver');
-const { Builder, By, until } = require("selenium-webdriver");
+const webdriver = require('selenium-webdriver');
 const chai = require("chai");
 const expect = chai.expect;
 
 
-describe("Login Test", () => {
-    it("Should display error for non-existent user login", async () => {
-        let driver = await new Builder().forBrowser("chrome").build();
 
-        try {
-            await driver.get("http://localhost:3000/login");
+describe('Login Page', function() {
+    this.timeout(5000); // Set timeout to 5 seconds
+    let driver;
+    let page;
 
-            await driver.findElement(By.id("username")).sendKeys("nonexistent_user");
-            await driver.findElement(By.id("password")).sendKeys("incorrect_password");
-            await driver.findElement(By.id("btnLogin")).click();
+    before(async function() {
+        driver = new webdriver.Builder().forBrowser('chrome').build();
+        page = await driver.get('http://localhost:3000/login'); 
+    });
 
-            let errorMessage;
-            try {
-                errorMessage = await driver.wait(
-                    until.elementLocated(By.css(".alert.alert-danger")),
-                    10000,
-                    "Error message element not found"
-                );
-                await driver.wait(
-                    until.elementIsVisible(errorMessage),
-                    10000,
-                    "Error message element not visible"
-                );
-            } catch (error) {
-                throw new Error("Error in locating or waiting for error message");
-            }
+    it('should have the correct title', async function() {
+        const title = await driver.getTitle();
+        expect(title).to.equal('Login'); 
+    });
 
-            let errorMessageText = await errorMessage.getText();
+    it('should allow user to login with valid credentials', async function() {
+        const usernameInput = await driver.findElement(webdriver.By.id('username')); 
+        const passwordInput = await driver.findElement(webdriver.By.id('password')); 
+        const loginButton = await driver.findElement(webdriver.By.id('btnLogin')); 
 
-            chai.assert.equal(errorMessageText.trim(), "Error: Could not login");
-        } finally {
-            await driver.quit();
-        }
-    }).timeout(30000);
+        await usernameInput.sendKeys('testuser');
+        await passwordInput.sendKeys('testpass');
+        await loginButton.click();
 
-    // it("Should successfully log in with valid credentials", async () => {
-    //     const username = process.env.TEST_ACCOUNT_USERNAME;
-    //     const password = process.env.TEST_ACCOUNT_PASSWORD;
+        // Wait for the page to load
+        await driver.wait(webdriver.until.urlContains('/jobroles'), 5000);
 
-    //     if (!username) {
-    //         throw new Error("TEST_ACCOUNT_USERNAME is not set in the environment variables");
-    //     }
+        // Check that the current URL contains '/jobroles'
+        const currentUrl = await driver.getCurrentUrl();
+        expect(currentUrl).to.include('/jobroles');
+    });
 
-    //     if (!password) {
-    //         throw new Error("TEST_ACCOUNT_PASSWORD is not set in the environment variables");
-    //     }
-
-    //     let driver = await new Builder().forBrowser("chrome").build();
-
-    //     try {
-    //         await driver.get("http://localhost:3000/login");
-
-    //         await driver.findElement(By.id("username")).sendKeys(username);
-    //         await driver.findElement(By.id("password")).sendKeys(password); 
-    //         await driver.findElement(By.id("btnLogin")).click();
-
-    //         let currentURL = await driver.getCurrentUrl();
-    //         console.log(currentURL);
-    //         chai.assert.isTrue(currentURL.includes("/jobroles"), "Login not successful. Redirect URL incorrect.");
-    //     } finally {
-    //         await driver.quit();
-    //     }
-    // }).timeout(25000); 
+    after(async function() {
+        await driver.quit();
+    });
 });
-
