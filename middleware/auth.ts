f750298ce7a4ffd03c313627a6c4eb1dadc4df6d
johnmcg04 <john.mcglinchey@kainos.com>
2023-12-28 12:50:54 +0000

    minor changes

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+const authService = require("../service/AuthService");
+
 
 module.exports = function (req: Request, res: Response, next: NextFunction) {
     if (req.session.token && req.session.token.length > 0) {
