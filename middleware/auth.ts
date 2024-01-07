e17aa9313ddca89999e7523a5636dfd8f161eb1d
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-07 00:20:19 +0000

    removed commented code

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,18 +1,6 @@
 import { NextFunction, Request, Response } from "express"; 
 const authService = require("../service/AuthService");
 
-
-// module.exports = function (req: Request, res: Response, next: NextFunction) {
-//     if (req.session.token && req.session.token.length > 0) {
-//         next();
-//     } 
-//     //need to ensure user cannot access admin webpages
-
-//     else {
-//         res.redirect("/login");
-//     }    
-// };
-
 module.exports = function (req: Request, res: Response, next: NextFunction) {
     if (req.session.token && req.session.token.length > 0) {
         // Check if the user has the necessary permissions to access the requested page
