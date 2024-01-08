0a671f714169f4c152b9ca2fe42857d8b9d1faba
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-08 21:29:50 +0000

    Fixed redirecting to sign up page issue in middle ware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,6 +2,10 @@ import { NextFunction, Request, Response } from "express";
 const authService = require("../service/AuthService");
 
 module.exports = function (req: Request, res: Response, next: NextFunction) {
+    if (req.url.startsWith('/signup') && !req.session.isAdmin) {
+        res.redirect('/signup');
+        return;
+    }
     if (req.session.token && req.session.token.length > 0) {
         // Check if the user has the necessary permissions to access the requested page
         if (req.url.startsWith('/admin') && !req.session.isAdmin) {
