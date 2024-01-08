59b63bb5f632544583ce0ce4785634e99e2c29f7
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-08 21:28:04 +0000

    Problems with redirecting to signup page

--- controller/SignUpController.ts
+++ controller/SignUpController.ts
@@ -1,26 +1,23 @@
 import { Request, Response, Application } from "express"
-import { Login } from "../model/auth"
-const authService = require("../service/SignUpService");
+import { SignUp } from "../model/auth"
+const signUpService = require("../service/SignUpService");
 
 module.exports = function(app: Application) {
 
-    app.get('/login', async (req: Request, res: Response) => {
-        res.render('login')
+    app.get('/signup', async (req: Request, res: Response) => {
+        res.render('signup')
     });
 
-    app.post('/login', async (req: Request, res: Response) => {
-        let data: Login = req.body
+    app.post('/signup', async (req: Request, res: Response) => {
+        let data: SignUp = req.body
 
         try{
-            req.session.token = await authService.login(data); //checking is valid log in returns UUID token
-            //let isAdmin = await authService.chkAdmin(req.session.token); //checking if token is admin returns bool
-            req.session.isAdmin = await authService.chkAdmin(req.session.token);
-            redirectToMenu(req.session.isAdmin, res);
+            req.session.token = await signUpService.signUp(data); //checking is valid sign up
         }
             catch(e){
             console.log(e)
             res.locals.errormessage = e.message
-            res.render('login', req.body)
+            res.render('signup', req.body)
         }
     });
 }
---
+++ service/SignUpService.ts
@@ -0,0 +1,13 @@
+import { SignUp } from "../model/auth";
+import { Request, Response, Application } from "express"
+const axios = require("axios");
+
+module.exports.signUp = async function (signUp: SignUp): Promise<void> {
+    try { 
+        const response = await axios.post("http://localhost:8080/api/signUp", signUp); //sending sign up info to API
+        
+        return response.data; //returning back the response of the sign up API endpoint
+    } catch (e) {
+        throw new Error("Could not sign up");
+    }
+};
