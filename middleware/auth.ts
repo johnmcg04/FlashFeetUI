9d0691c338049b35275ace90b9306b9347e1d3e1
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-08 21:42:05 +0000

    Updated middleware slightly

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,11 +2,11 @@ import { NextFunction, Request, Response } from "express";
 const authService = require("../service/AuthService");
 
 module.exports = function (req: Request, res: Response, next: NextFunction) {
-    if (req.url.startsWith('/signup')) {
+    if (req.url == '/signup') {
         res.redirect('/signup');
         return;
     }
-    else if (req.session.token && req.session.token.length > 0) {
+    if (req.session.token && req.session.token.length > 0) {
         // Check if the user has the necessary permissions to access the requested page
         if (req.url.startsWith('/admin') && !req.session.isAdmin) {
             res.redirect('/unauthorized');
