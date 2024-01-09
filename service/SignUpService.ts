ea858be76df9ecb231df68cd999129915ee0be7a
johnmcg04 <john.mcglinchey@kainos.com>
2024-01-09 21:46:14 +0000

    Added password validation on front end, error messages are specific to the input data

--- service/SignUpService.ts
+++ service/SignUpService.ts
@@ -2,10 +2,6 @@ import { SignUp } from "../model/SignUp"
 import { Request, Response, Application } from "express"
 const axios = require("axios");
 
-interface SignUp {
-  // Define your SignUp interface properties here
-}
-
 export const signUp = async function (signUp: SignUp): Promise<void> {
   const passwordValidationResult = validateSignUpPassword(signUp.password);
 
@@ -18,11 +14,10 @@ export const signUp = async function (signUp: SignUp): Promise<void> {
   try {
     const response = await axios.post("http://localhost:8080/api/signup", signUp);
     // Handle the successful sign-up response as needed
-    console.log(response.data); // For example, log the response data
     
   } catch (e) {
     throw new Error("Could not sign up");
-  }
+  }     
 };
 
 // Password validation function
@@ -31,27 +26,27 @@ function validateSignUpPassword(password: string): string[] {
 
   // Check for at least 8 characters
   if (password.length < 8) {
-    errors.push("Password needs to be a minimum of 8 characters");
+    errors.push("Password needs to be a minimum of 8 characters.");
   }
 
   // Check for at least 1 special character
   if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
-    errors.push("Password must contain at least 1 special character");
+    errors.push("Password must contain at least 1 special character.");
   }
 
   // Check for at least 1 upper case letter
   if (!/[A-Z]/.test(password)) {
-    errors.push("Password must contain at least 1 upper case letter");
+    errors.push("Password must contain at least 1 upper case letter.");
   }
 
   // Check for at least 1 lower case letter
   if (!/[a-z]/.test(password)) {
-    errors.push("Password must contain at least 1 lower case letter");
+    errors.push("Password must contain at least 1 lower case letter.");
   }
 
   // Check for at least 1 digit
   if (!/\d/.test(password)) {
-    errors.push("Password must contain at least 1 digit");
+    errors.push("Password must contain at least 1 digit.");
   }
 
   return errors;
