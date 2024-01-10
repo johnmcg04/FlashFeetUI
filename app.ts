95a0c16741879b27bfdd3362a6b69c299cda9dd9
johnmcg04 <145208852+johnmcg04@users.noreply.github.com>
2024-01-10 16:04:33 +0000

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,29 +1,19 @@
-import type { JobRole } from "./model/jobrole";
-import type { Request, Response } from "express";
+import { Response, Request } from 'express';
+import { Login } from './model/auth';
 
-const express = require("express");
+const express = require('express');
 const path = require("path");
 const nunjucks = require("nunjucks");
+const { title } = require('process');
+const session = require('express-session');
+process.env['SESSION_SECRET'] = 'your_secret_here'
 
 const app = express();
 
+//configure nunjucks
+let appViews = path.join(__dirname, '/views/');
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
- 
-app.use(express.json())
- 
-app.use(express.urlencoded({ extended: true}))
-
-declare module "express-session" {
-    interface SessionData {
-        jobRole: JobRole
-    }
-}
-
-// Configure Nunjucks
-const appViews = path.join(__dirname, "/views/");
-
-const nunjucksConfig = {
+let nunjucksConfig = {
     autoescape: true,
     noCache: true,
     express: app
@@ -31,18 +21,53 @@ const nunjucksConfig = {
 
 nunjucks.configure(appViews, nunjucksConfig);
 
-// Configure Express
-app.set("view engine", "html");
+//configure express
+
+app.set('view engine', 'html');
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(session(
+        {   
+            secret:'not hard coded secret', 
+            cookie:{ maxAge: 60000 }
+        }
+    )
+);
+
+declare module "express-session" {
+    interface SessionData{
+        token: string;
+        isAdmin: boolean;
+    }
+}
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
 });
 
-// Express Routes
-app.get("/", (req: Request, res: Response) => {
-    res.render("index");
+
+//express routes
+app.get('/', (req: Request, res: Response) => {
+    res.render("login", {
+        title: 'Login Or Sign Up',
+    });     
 });
 
+require('./controller/jobroleController')(app);
 
-require('./controller/jobroleController')(app)
+require('./controller/authController')(app);
 
+require('./controller/SignUpController')(app);
+
+require('./controller/adminController')(app);
+
+require('./controller/menuController')(app);
+
+const authMiddleware = require("./middleware/auth");
+app.use(authMiddleware);
